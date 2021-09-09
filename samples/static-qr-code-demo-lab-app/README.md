e5652133a3a5779d643c3ae7cc3cae2d8255dba2
Marijana Stanisavljević <mrjst1995@gmail.com>
2021-09-09 10:20:08 +0000

    Demo lab app static qr code

---
+++ samples/static-qr-code-demo-lab-app/README.md
@@ -0,0 +1,91 @@
+## Verity Sample Static QR Code Lab App
+
+This is a sample application that demonstrates Lab App API integration with a [static QR code approach](https://gitlab.com/evernym/verity/lab-app-integration-guide#option-3-static-qr-codes).
+>DISCLAIMER: This is a sample application, and it isn't meant to be used in production. There are some disadvantages like ngrok tunnels that time out, and in-memory storage that stores user data only while this app is up and running. 
+
+### Launching the application Locally
+
+Requirements:
+- You have received Domain DID, REST API key and Credential Definition Id from Evernym
+- You have NodeJs v12 or later installed
+- You have ngrok installed ([https://ngrok.com/](https://ngrok.com/)).
+> NOTE: Ngrok is used to create a public webhook URL which will forward response messages from Verity Application Server to the web app. If you have capabilities to start the application on a public IP address then you don't need ngrok]
+
+To try out the application follow these steps:
+
+- In a separate terminal window start ngrok for port 4000 and leave it running:
+```sh
+ngrok http 4000
+```
+- Install required NodeJs packages:
+```sh
+npm install
+```
+- Start the app
+```sh
+node app.js
+```
+
+- Fill in values for VERITY_URL, DOMAIN_DID, X_API_KEY (you should have received these inputs from Evernym), and WEBHOOK_URL into **.env** file in the current folder. A properly filled **.env** file should have this format:
+```sh
+VERITY_URL=https://vas.pps.evernym.com
+DOMAIN_DID=RvhfGuPU86SqgNzFKaK8ou
+X_API_KEY=3VB7DfTwDptvWusaQeL3sEbdbtbDskceLcLk3m3Xrw7e:2ijiJEpbYkERCcJUGSfX2wjsX22WTzhJXBmqqm8e35FWZ4fjyLMBfVE92mYJJ72CUUTRg4ZT2LxFHTzGntembzjK
+CRED_DEF_ID=282aNwH4oHuqq3rg911TVB:3:CL:179684:latest
+WEBHOOK_URL=https://2cdfa95b3ed5.ngrok.io/webhook
+```
+> NOTE: These are just sample reference values. These values will NOT work if left unchanged. You should specify DOMAIN_DID, X_API_KEY and CRED_DEF_ID that you received from Evernym
+
+The application will be available on http://localhost:4000
+
+Generate Static QR Code and scan it with Camera app or some QR Code Scanner app. [Here](#generating-static-qr-code) you can see how to generate a QR Code.
+
+### Launching the application using Docker
+
+Requirements:
+- You have received Domain DID, REST API key and Credential Definition Id from Evernym
+- You have Docker installed
+
+To start the app follow these steps:
+- Fill in values for VERITY_URL, DOMAIN_DID and X_API_KEY (you should have received these inputs from Evernym) into **.env** file in the current folder. A properly filled **.env** file should have this format:
+```sh
+VERITY_URL=https://vas.pps.evernym.com
+DOMAIN_DID=RvhfGuPU86SqgNzFKaK8ou
+X_API_KEY=3VB7DfTwDptvWusaQeL3sEbdbtbDskceLcLk3m3Xrw7e:2ijiJEpbYkERCcJUGSfX2wjsX22WTzhJXBmqqm8e35FWZ4fjyLMBfVE92mYJJ72CUUTRg4ZT2LxFHTzGntembzjK
+CRED_DEF_ID=282aNwH4oHuqq3rg911TVB:3:CL:179684:latest
+```
+> NOTE: These are just sample reference values. These values will NOT work if left unchanged. You should specify DOMAIN_DID, X_API_KEY and CRED_DEF_ID that you received from Evernym
+- Create a docker image with pre-installed requirements. Run this command from the current folder:
+```sh
+docker build -t static-qr-code-demo-lab-app .
+```
+Start a Docker container:
+```sh
+docker run -p 4000:4000 --env-file .env -it static-qr-code-demo-lab-app
+```
+
+The application will be available on http://localhost:4000
+
+Generate Static QR Code and scan it with Camera app or some QR Code Scanner app. [Here](#generating-static-qr-code) you can see how to generate a QR Code.
+
+
+### Generating Static QR Code
+
+A static QR code should be created from the URL in the following format:
+
+For PROD Travel Pass:
+- https://home.prod.travelpassinfra.org/?itp_url=<staticURL>&sender=sender&route=1
+
+For Staging Travel Pass:
+- https://home.staging.travelpassinfra.org/?itp_url=<staticURL>&sender=sender&route=1
+
+In this app, as it uses Staging Net, static QR Code URL should be created like this:
+- https://home.staging.travelpassinfra.org/?itp_url=<your_ngrok_address/generate-proof-request>&sender=sender&route=1
+
+Example of static QR Code URL:
+- https://home.staging.travelpassinfra.org/?itp_url=https://ae25-178-220-107-20.ngrok.io/generate-proof-request&sender=sender&route=1
+
+You can use any QR code generator (e.g. https://www.qr-code-generator.com/).
+
+
+© 2013-2021, ALL RIGHTS RESERVED, EVERNYM INC.
---
+++ samples/static-qr-code-demo-lab-app/app.js
@@ -0,0 +1,388 @@
+'use strict'
+
+const express = require('express')
+const axios = require('axios')
+const path = require('path')
+const uuid4 = require('uuid4')
+const urljoin = require('url-join')
+const dotenv = require('dotenv')
+const expressLayouts = require('express-ejs-layouts')
+dotenv.config()
+
+const ANSII_GREEN = '\u001b[32m'
+const ANSII_RESET = '\x1b[0m'
+const PORT = 4000
+
+const verityUrl = process.env.VERITY_URL // address of Verity Application Service
+const domainDid = process.env.DOMAIN_DID // your Domain DID on the multi-tenant Verity Application Service
+const xApiKey = process.env.X_API_KEY // REST API key associated with your Domain DID
+const webhookUrl = process.env.WEBHOOK_URL // public URL for the webhook endpoint
+const credDefId = process.env.CRED_DEF_ID // credential definition that you recieved
+
+// Sends a message to the Verity Application Service via the Verity REST API
+async function sendVerityRESTMessage (qualifier, msgFamily, msgFamilyVersion, msgName, message, threadId) {
+  // qualifier - either 'BzCbsNYhMrjHiqZDTUASHg' for Aries community protocols or '123456789abcdefghi1234' for Evernym-specific protocols
+  // msgFamily - message family (e.g. 'present-proof')
+  // msgFamilyVersion - version of the message family (e.g. '1.0')
+  // msgName - name of the protocol message to perform (e.g. 'request')
+  // message - message to be sent in the body payload
+  // threadId - unique identifier of the protocol interaction. The threadId is used to distinguish between simultaenous interactions
+
+  // Add @type and @id fields to the message in the body payload
+  // Field @type is dinamycially constructed from the function arguments and added into the message payload
+  message['@type'] = `did:sov:${qualifier};spec/${msgFamily}/${msgFamilyVersion}/${msgName}`
+  message['@id'] = uuid4()
+
+  if (!threadId) {
+    threadId = uuid4()
+  }
+
+  // send prepared message to Verity and return Axios request promise
+  const url = urljoin(verityUrl, 'api', domainDid, msgFamily, msgFamilyVersion, threadId)
+  console.log(`Posting message to ${ANSII_GREEN}${url}${ANSII_RESET}`)
+  console.log(`${ANSII_GREEN}${JSON.stringify(message, null, 4)}${ANSII_RESET}`)
+  return axios({
+    method: 'POST',
+    url: url,
+    data: message,
+    headers: {
+      'X-API-key': xApiKey // <-- REST API Key is added in the header
+    }
+  })
+}
+
+// Save invite URL
+const inviteURLMap = new Map()
+// Store a relationship DID for a user that you're currently connecting to
+let currentRelationshipDID = null
+// Store generated relationship DID
+let relationshipDID
+// Update configs
+const updateConfigsMap = new Map()
+// Relationship create
+const relCreateMap = new Map()
+// Proof request
+const proofRequestMap = new Map()
+// Retrieved User Data Map
+// This is a DEMO application, and user Data is stored in-memory
+// Once the app stops, all of the data is gone
+const userDataMap = new Map()
+// Conect relationship test sample id to relationship did
+const didTestSampeMap = new Map()
+// Issue Credential
+const issueCredentialMap = new Map()
+
+// Update webhook protocol is synchronous and does not support threadId
+// update webhook endpoint on app startup
+let webhookResolve
+async function initialize () {
+  const webhookMessage = {
+    comMethod: {
+      id: 'webhook',
+      type: 2,
+      value: webhookUrl,
+      packaging: {
+        pkgType: 'plain'
+      }
+    }
+  }
+
+  const updateWebhook =
+    new Promise(function (resolve, reject) {
+      webhookResolve = resolve
+      sendVerityRESTMessage('123456789abcdefghi1234', 'configs', '0.6', 'UPDATE_COM_METHOD', webhookMessage)
+    })
+  await updateWebhook
+
+  // Update configuration
+  // Add your logo and lab name here
+  const updateConfigMessage = {
+    configs: [
+      {
+        name: 'logoUrl',
+        value: 'https://freeiconshop.com/icon/eye-icon-flat/'
+      },
+      {
+        name: 'name',
+        value: 'Example Lab'
+      }
+    ]
+  }
+
+  const updateConfigsThreadId = uuid4()
+  const updateConfigs =
+    new Promise(function (resolve, reject) {
+      updateConfigsMap.set(updateConfigsThreadId, resolve)
+    })
+
+  await sendVerityRESTMessage('123456789abcdefghi1234', 'update-configs', '0.6', 'update', updateConfigMessage, updateConfigsThreadId)
+
+  await updateConfigs
+}
+// Create relationship key
+async function createRelationshipDid () {
+  // Add logo and lab name here
+  const relationshipCreateMessage = {}
+  const relThreadId = uuid4()
+  const relationshipCreate =
+    new Promise(function (resolve, reject) {
+      relCreateMap.set(relThreadId, resolve)
+    })
+
+  await sendVerityRESTMessage('123456789abcdefghi1234', 'relationship', '1.0', 'create', relationshipCreateMessage, relThreadId)
+  const relationshipDid = await relationshipCreate
+  return relationshipDid
+}
+
+// Proof request to identify user
+async function requestProof () {
+  const relationshipDid = await createRelationshipDid()
+  const proofMessage = {
+    '~for_relationship': relationshipDid,
+    name: 'Travel Pass Id',
+    proof_attrs: [
+      {
+        name: 'givenNames',
+        restrictions: [],
+        self_attest_allowed: true
+      },
+      {
+        name: 'lastName',
+        restrictions: [],
+        self_attest_allowed: true
+      },
+      {
+        name: 'number',
+        restrictions: [],
+        self_attest_allowed: true
+      }
+    ],
+    proof_predicates: [],
+    by_invitation: true
+  }
+  const proofThreadId = uuid4()
+  await sendVerityRESTMessage('BzCbsNYhMrjHiqZDTUASHg', 'present-proof', '1.0', 'request', proofMessage, proofThreadId)
+  const inviteURL =
+    new Promise(function (resolve, reject) {
+      inviteURLMap.set(relationshipDID, resolve)
+    })
+  const inviteURLValue = await inviteURL
+  return (inviteURLValue)
+}
+
+async function validateProof (validationResult) {
+  const requestProof =
+    new Promise(function (resolve, reject) {
+      proofRequestMap.set(relationshipDID, resolve)
+    })
+  proofRequestMap.get(relationshipDID)(validationResult)
+  const verificationResult = await requestProof
+  if (verificationResult === 'ProofValidated') {
+    console.log('Proof is validated!')
+    return true
+  } else {
+    console.log('Proof is NOT validated')
+    return false
+  }
+}
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.get('/users', function (req, res) {
+  console.log('REQUEST')
+  console.log(userDataMap)
+  res.render('users', { usersDataMap: userDataMap })
+})
+
+// Add test sample id
+app.post('/user-registration/:did', function (req, res) {
+  const testSampleId = req.body.test_sample
+  const did = req.params.did
+  const userData = userDataMap.get(did)
+  userDataMap.set(did, {
+    givenNames: userData.givenNames,
+    lastName: userData.lastName,
+    passportNumber: userData.passportNumber,
+    testSampleId: testSampleId,
+    resultIssued: false
+  })
+  const now = new Date().toISOString()
+  didTestSampeMap.set(testSampleId, { did: did, taken_date: now })
+  currentRelationshipDID = null
+  res.redirect('/users')
+})
+
+// Change test sample id
+app.post('/user-update/:did', function (req, res) {
+  const testSampleId = req.body.test_sample
+  const did = req.params.did
+  const userData = userDataMap.get(did)
+  const oldTestSampleId = userData.testSampleId
+  userDataMap.set(did, {
+    givenNames: userData.givenNames,
+    lastName: userData.lastName,
+    passportNumber: userData.passportNumber,
+    testSampleId: testSampleId,
+    resultIssued: false
+  })
+  const now = new Date().toISOString()
+  didTestSampeMap.delete(oldTestSampleId)
+  didTestSampeMap.set(testSampleId, { did: did, taken_date: now })
+  currentRelationshipDID = null
+  res.redirect('/users')
+})
+
+// Send test results
+app.get('/send-test-results', async (req, res) => {
+  res.render('sendTestResults', { testSamplesMap: didTestSampeMap })
+})
+app.post('/send-test-results', async (req, res) => {
+  const now = new Date().toISOString()
+  const didAndTime = req.body.test_sample_did
+  const values = didAndTime.split(' ')
+  const userData = userDataMap.get(values[0])
+  const credentialData = {
+    takenDate: values[1],
+    resultDate: now,
+    testMethod: req.body.test_method,
+    labName: req.body.lab_name,
+    labCode: req.body.lab_code,
+    testLanguage: req.body.test_language,
+    testType: req.body.test_type,
+    testTechnique: req.body.test_technique,
+    testFormat: 'Digital',
+    testResult: req.body.test_result
+  }
+
+  const credentialMessage = {
+    '~for_relationship': values[0],
+    cred_def_id: credDefId,
+    credential_values: credentialData,
+    comment: 'COVID-19 Test',
+    by_invitation: false,
+    auto_issue: true,
+    price: 0
+  }
+  const issueCredThreadId = uuid4()
+
+  const credentialOffer =
+    new Promise(function (resolve, reject) {
+      issueCredentialMap.set(issueCredThreadId, resolve)
+    })
+
+  await sendVerityRESTMessage('BzCbsNYhMrjHiqZDTUASHg', 'issue-credential', '1.0', 'offer', credentialMessage, issueCredThreadId)
+  await credentialOffer
+  userDataMap.set(values[0], {
+    givenNames: userData.givenNames,
+    lastName: userData.lastName,
+    passportNumber: userData.passportNumber,
+    testSampleId: userData.testSampleId,
+    resultIssued: true
+  })
+  didTestSampeMap.delete(values[2])
+  res.redirect('/')
+})
+
+// Return invite URL when a user clicks on deep link
+app.get('/generate-proof-request/', async (req, res) => {
+  const inviteurl = await requestProof()
+  res.redirect(`${inviteurl}`)
+})
+// Receive messages on webhook
+app.post('/webhook', async (req, res) => {
+  const message = req.body
+  const threadId = message['~thread'] ? message['~thread'].thid : null
+  let retrievedGivenNames
+  let retrievedLastName
+  let retrievedPassportNumber
+  console.log('Got message on the webhook')
+  console.log(`${ANSII_GREEN}${JSON.stringify(message, null, 4)}${ANSII_RESET}`)
+  res.status(202).send('Accepted')
+  // Handle received message differently based on the message type
+  switch (message['@type']) {
+    case 'did:sov:123456789abcdefghi1234;spec/configs/0.6/COM_METHOD_UPDATED':
+      webhookResolve('webhook updated')
+      break
+    case 'did:sov:123456789abcdefghi1234;spec/update-configs/0.6/status-report':
+      updateConfigsMap.get(threadId)('config updated')
+      break
+    case 'did:sov:123456789abcdefghi1234;spec/relationship/1.0/created':
+      relCreateMap.get(threadId)(message.did)
+      relationshipDID = message.did
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/request-received':
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/response-sent':
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/sent-response':
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/sent':
+      if (message.msg['credentials~attach']) {
+        issueCredentialMap.get(threadId)('credential issued')
+      }
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/presentation-result':
+      if (currentRelationshipDID === null) {
+        currentRelationshipDID = relationshipDID
+      }
+      const verificationResult = await validateProof(message.verification_result)
+      if (verificationResult === true) {
+        if (typeof message.requested_presentation.revealed_attrs.givenNames !== 'undefined') {
+          retrievedGivenNames = message.requested_presentation.revealed_attrs.givenNames.value
+        } else {
+          retrievedGivenNames = message.requested_presentation.self_attested_attrs.givenNames
+        }
+        if (typeof message.requested_presentation.revealed_attrs.lastName !== 'undefined') {
+          retrievedLastName = message.requested_presentation.revealed_attrs.lastName.value
+        } else {
+          retrievedLastName = message.requested_presentation.self_attested_attrs.lastName
+        }
+        if (typeof message.requested_presentation.revealed_attrs.number !== 'undefined') {
+          retrievedPassportNumber = message.requested_presentation.revealed_attrs.number.value
+        } else {
+          retrievedPassportNumber = message.requested_presentation.self_attested_attrs.number
+        }
+        userDataMap.set(currentRelationshipDID, {
+          givenNames: retrievedGivenNames,
+          lastName: retrievedLastName,
+          passportNumber: retrievedPassportNumber,
+          testSampleId: null,
+          resultIssued: false
+        })
+        relationshipDID = null
+        currentRelationshipDID = null
+      }
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/protocol-invitation':
+      inviteURLMap.get(relationshipDID)(message.inviteURL)
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/problem-report':
+      if (message.description.code === 'rejection') {
+        console.log('User rejected to share data')
+      }
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/out-of-band/1.0/move-protocol':
+      break
+    case 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/out-of-band/1.0/relationship-reused':
+      currentRelationshipDID = message.relationship
+      break
+    default:
+      console.log(`Unexpected message type ${message['@type']}`)
+  }
+})
+
+app.listen(PORT, async () => {
+  console.log('Initializing the app...')
+  await initialize()
+  console.log(`Webhook listening on port ${PORT}`)
+  console.log('Demo lab app now available at http://localhost:4000/')
+})
